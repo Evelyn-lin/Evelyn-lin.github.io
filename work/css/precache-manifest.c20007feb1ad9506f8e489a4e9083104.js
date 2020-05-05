ee0120b149d2e7b6adc46a797147a4ec4a3f5326
Evelyn <1062445894@qq.com>
2020-05-05 12:46:05 +0800

    Site updated: 2020-05-05 12:45:36

---
+++ work/css/precache-manifest.c20007feb1ad9506f8e489a4e9083104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ac708dfbf1bdadb33d7e501637add5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "723f0dbda700bc896cd9",
+    "url": "./static/css/2.435c6fcb.chunk.css"
+  },
+  {
+    "revision": "4792fd1b966eaaf95730",
+    "url": "./static/css/main.0fadfd30.chunk.css"
+  },
+  {
+    "revision": "723f0dbda700bc896cd9",
+    "url": "./static/js/2.b2bcafbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.b2bcafbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4792fd1b966eaaf95730",
+    "url": "./static/js/main.a3ed9d32.chunk.js"
+  },
+  {
+    "revision": "90a5b1a5b5cb725e980f",
+    "url": "./static/js/runtime-main.0056c7a8.js"
+  }
+]);
---
+++ work/musicApp/precache-manifest.678d566e9e0b8075941361b1a50d424a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8462b051649797c82a1b37bff35ab7ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1069f25046a7b767c068",
+    "url": "./static/css/2.1dc9f3c9.chunk.css"
+  },
+  {
+    "revision": "741a23d473a8ac66741a",
+    "url": "./static/css/main.c57335f9.chunk.css"
+  },
+  {
+    "revision": "1069f25046a7b767c068",
+    "url": "./static/js/2.0cec43a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.0cec43a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741a23d473a8ac66741a",
+    "url": "./static/js/main.10c38d89.chunk.js"
+  },
+  {
+    "revision": "90a5b1a5b5cb725e980f",
+    "url": "./static/js/runtime-main.0056c7a8.js"
+  }
+]);
